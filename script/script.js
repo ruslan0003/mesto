1d4cba56650611a8c3d752d241ed6d8ecdc0971f
Ruslan Lukichev <ruslan0003@yandex.ru>
2020-06-18 09:15:10 +0300

    refactor: Убрать переменные в начало скрипта

--- script/script.js
+++ script/script.js
@@ -1,6 +1,14 @@
 const popup = document.querySelector('.pop-up');
 const popupOpen = document.querySelector('.profile__edit-button');
 const popupClose = popup.querySelector('.pop-up__close-button');
+const popupSubmitClose = popup.querySelector('.pop-up__submit-button');
+
+let formElement = document.querySelector('.pop-up__form');
+let nameInput = formElement.querySelector('.pop-up__form-field_input_name');
+let jobInput = formElement.querySelector('.pop-up__form-field_input_job');
+let nameOutput = document.querySelector('.profile__name');
+let jobOutput = document.querySelector('.profile__position');
+
 
 const popupToggle = function () {
   popup.classList.toggle('pop-up_opened');
@@ -9,48 +17,25 @@ const popupToggle = function () {
 popupOpen.addEventListener('click', popupToggle);
 popupClose.addEventListener('click', popupToggle);
 
-  // Находим форму в DOM
-let formElement = document.querySelector('.pop-up__form')// Воспользуйтесь методом querySelector()
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    let nameInput = formElement.querySelector('.pop-up__form-field_input_name');// Воспользуйтесь инструментом .querySelector()
-    let jobInput = formElement.querySelector('.pop-up__form-field_input_job');// Воспользуйтесь инструментом .querySelector()
-
-    // Получите значение полей из свойства value
+    evt.preventDefault();
 
     let nameInputValue = nameInput.value;
     let jobInputValue = jobInput.value;
 
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    let nameInputField = document.querySelector('.profile__name');
-    let jobInputField = document.querySelector('.profile__position');
-
-    // Вставьте новые значения с помощью textContent
-
     if (nameInputValue == "") {
-      nameInputField.textContent = "Жак-Ив Кусто";
-      jobInputField.textContent = "Исследователь океана";
+      nameOutput.textContent = "Жак-Ив Кусто";
+      jobOutput.textContent = "Исследователь океана";
     }
 
     else {
-      nameInputField.textContent = nameInputValue;
-      jobInputField.textContent = jobInputValue;
+      nameOutput.textContent = nameInputValue;
+      jobOutput.textContent = jobInputValue;
     }
 
   }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
-
-const popupSubmitClose = popup.querySelector('.pop-up__submit-button');
 popupSubmitClose.addEventListener('click', popupToggle);
 
