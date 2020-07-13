4cfbd58509c59ac8256e9b66a74d9761940f2d37
Ruslan Lukichev <ruslan0003@yandex.ru>
2020-07-13 12:10:06 +0300

    feature: Сделать контроль состояния кнопки submit

--- script/validate.js
+++ script/validate.js
@@ -2,8 +2,8 @@
 
 const showInputError = (form, input, errorMessage) => {
   const formErrorMessage = form.querySelector(`#${input.id}-error`);
-  input.classList.add('popup-edit__form-field_type_error');
-  formErrorMessage.classList.add('popup-edit__input-error_active');
+  input.classList.add('form__form-field_type_error');
+  formErrorMessage.classList.add('form__input-error_active');
   formErrorMessage.textContent = errorMessage;
 }
 
@@ -11,9 +11,27 @@ const showInputError = (form, input, errorMessage) => {
 
 const hideInputError = (form, input) => {
   const formErrorMessage = form.querySelector(`#${input.id}-error`);
-  input.classList.remove('popup-edit__form-field_type_error');
-  formErrorMessage.classList.remove('popup-edit__input-error_active');
+  input.classList.remove('form__input_type_error');
+  formErrorMessage.classList.remove('form__input-error_active');
   formErrorMessage.textContent = '';
+}
+
+//функция проверки полей формы на валидность - возвращает true, если хотя бы одно поле невалидно
+
+const hasInvalidInput = (allInputList) => {
+  return allInputList.some((input) => {
+    return !input.validity.valid;
+  });
+}
+
+//функция изменения класса у кнопки submit - активное/неактивное состояние
+
+const toggleButtonState = (allInputList, button) => {
+  if (hasInvalidInput(allInputList)) {
+    button.classList.add('form__submit_inactive');
+  } else {
+    button.classList.remove('form__submit_inactive');
+  }
 }
 
 //функция проверки валидности полей в формах
@@ -27,25 +45,30 @@ function isFormValid (form, input) {
   }
 }
 
-//функция добавления слушателей событий всем полям ввода через служебный класс popup-form__input
+//функция добавления слушателей событий всем полям ввода
 
 const setEventListeners = (form) => {
   //cоздаем массив всех инпутов внутри формы
-  const allInputList = Array.from(form.querySelectorAll('.popup-form__input'));
+  const allInputList = Array.from(form.querySelectorAll('.form__input'));
+  //находим кнопку submit в форме и вызываем функцию изменения состояния кнопки для её отключения при первоначальном открытии формы
+    const button = form.querySelector('.form__submit');
+  toggleButtonState (allInputList, button);
   //обходим коллекцию инпутов и каждому навешиваем обработчик события input
   allInputList.forEach((input) => {
     input.addEventListener('input', () => {
       //при вводе любого символа вызываем функцию isFormValid, передаём ей форму и поле ввода как аргументы
       isFormValid(form, input);
+      //вызываем функцию изменения состояния кнопки повторно, передаём массив инпутов и кнопку как аргументы
+      toggleButtonState (allInputList, button);
     });
   });
 }
 
-//функция добавления setEventListeners всем формам через служебный класс popup-form
+//функция добавления setEventListeners всем формам через служебный класс form
 
 function enableValidation() {
   //создаем массив всех форм
-  const allFormList = Array.from(document.querySelectorAll('.popup-form'));
+  const allFormList = Array.from(document.querySelectorAll('.form'));
   //обходим коллекцию форм, для каждой отменяем стандартное поведение submit, затем вызываем setEventListener с формой как аргументом
   allFormList.forEach((form) => {
     form.addEventListener('submit', (evt) => {
@@ -56,8 +79,3 @@ function enableValidation() {
 }
 
 enableValidation();
-
-
-//функция проверки полей формы на валидность - возвращает true, если хотя бы одно поле невалидно
-
-//функция изменения класса у кнопки submit - активное/неактивное состояние
